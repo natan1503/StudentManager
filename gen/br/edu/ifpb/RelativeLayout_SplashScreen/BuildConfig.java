0dca0c665d7578ae003b692bb7e6fb9cc7076a92
IFPB <IFPB@labredes-02>
2015-05-13 11:48:22 -0300

    Initial commit

---
+++ gen/br/edu/ifpb/RelativeLayout_SplashScreen/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package br.edu.ifpb.RelativeLayout_SplashScreen;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/br/edu/ifpb/RelativeLayout_SplashScreen/SplashActivity.java
@@ -0,0 +1,32 @@
+package br.edu.ifpb.RelativeLayout_SplashScreen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SplashActivity extends Activity implements Runnable {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+		
+		Handler SplashScren = new Handler();
+		SplashScren.postDelayed(SplashActivity.this, 3000);
+	}
+	
+	public void run(){
+		startActivity(new Intent(SplashActivity.this, MainActivity.class));
+		finish();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.splash, menu);
+		return true;
+	}
+}
